2cbac558c6fb77f28d2a937f8bf3fc1d2956d46f
jemma-mg <jemmamariyageorge@gmail.com>
2023-07-07 23:43:59 +0530

    changed urls

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def landing():
+    return render_template('about.html')
+
+@app.route('/user')
 def home():
     return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 
 if __name__ == "__main__":
